fe212c8a1479428033c521c87cbfdfc2c5936687
ben-yeung <benjaminyeung@berkeley.edu>
2022-04-26 23:26:28 -0700

    Fixing corner cases on summary command

--- src/commands/summary.js
+++ src/commands/summary.js
@@ -3,25 +3,32 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const guildSchema = require('../schemas/guild-schema');
 const sdk = require('api')('@opensea/v1.0#bg4ikl1mk428b');
 
-var desc = ''
 
 async function buildDesc(client, collects) {
+    let desc = '';
     for (let i = 0; i < collects.length; i++) {
         let c = collects[i];
-        let res = await sdk['retrieving-a-single-collection']({ collection_slug: c.slug });
 
-        let stats = res.collection.stats;
-        let currFloor = stats.floor_price;
-        let oneDayVol = Number((stats.one_day_volume).toFixed(0));
-        let oneDaySales = Number(stats.one_day_sales.toLocaleString("en-US"));
-        let royalty = (Number(res.collection.dev_seller_fee_basis_points) + Number(res.collection.opensea_seller_fee_basis_points)) / 10000;
-        let afterFees = currFloor - (royalty * currFloor);
+        try {
+            let res = await sdk['retrieving-a-single-collection']({ collection_slug: c.slug });
 
-        let name = res.collection.name;
-        let openSea = 'https://opensea.io/collection/' + res.collection.slug;
+            let stats = res.collection.stats;
+            let currFloor = stats.floor_price;
+            let oneDayVol = Number((stats.one_day_volume).toFixed(0));
+            let oneDaySales = Number(stats.one_day_sales.toLocaleString("en-US"));
+            let royalty = (Number(res.collection.dev_seller_fee_basis_points) + Number(res.collection.opensea_seller_fee_basis_points)) / 10000;
+            let afterFees = Number((currFloor - (royalty * currFloor)).toFixed(4));
 
-        desc += `\n\n*${name}* • [OpenSea](${openSea}) \n **Floor Price:** ${currFloor}Ξ (~$${client.convertETH(currFloor).toLocaleString("en-US")}) **After Fees:** ${afterFees}Ξ (~$${client.convertETH(afterFees).toLocaleString("en-US")}) \n **24H Volume:** ${oneDayVol.toLocaleString("en-US")}Ξ (~$${client.convertETH(oneDayVol).toLocaleString("en-US")}) **24H Sales:** ${oneDaySales}`
+            let name = res.collection.name;
+            let openSea = 'https://opensea.io/collection/' + c.slug;
+
+            desc += `\n\n*${name}* • [OpenSea](${openSea}) \n **Floor Price:** ${currFloor}Ξ (~$${client.convertETH(currFloor).toLocaleString("en-US")}) **After Fees:** ${afterFees}Ξ (~$${client.convertETH(afterFees).toLocaleString("en-US")}) \n **24H Volume:** ${oneDayVol.toLocaleString("en-US")}Ξ (~$${client.convertETH(oneDayVol).toLocaleString("en-US")}) **24H Sales:** ${oneDaySales}`
+        } catch (err) {
+            console.log(err);
+        }
+
     }
+    return desc;
 }
 
 module.exports = {
@@ -40,7 +47,7 @@ module.exports = {
             if (guildRes) {
                 let collects = guildRes.collections;
 
-                buildDesc(client, collects).then(() => {
+                buildDesc(client, collects).then((desc) => {
 
                     if (desc == '') desc = 'OpenSea API rate limited. Please try again later.'
 
@@ -48,16 +55,18 @@ module.exports = {
                         .setTitle('Summary')
                         .setDescription(desc)
                         .setColor(44774)
+                        .setTimestamp()
 
-                    return interaction.reply({ embeds: [embed] })
-                })
+                    return interaction.reply({ embeds: [embed] });
+                }).catch(err => console.log(err))
 
             }
 
         } catch (err) {
             console.log(err);
-            return interaction.reply('Something went wrong. Try again in a moment.')
+            return interaction.reply('Error occurred. Please try again in a moment.')
         }
+
 
     },
 }
